3b05421a3147c856dee4a25c36423322dc216ffc
wasim180 <wa4641319@gmail.com>
2025-11-20 12:27:36 +0530

    Correct even number printing logic in evenloop.c
    Fix logic to print even numbers and handle syntax error.

--- WASIM/loop and for/evenloop.c
+++ WASIM/loop and for/evenloop.c
@@ -1,11 +1,17 @@
 #include<stdio.h>
 int main(){
-    int a=2;
+    int n, a=2;
 // printing fifty even number.
     while(a<=50){
+            if(n%2==0){
         printf("\n%d",a);
         a=a+2;
     }
+    else{
+        printf("syntax errror");
+    }
+    }
+
 
 
 }
