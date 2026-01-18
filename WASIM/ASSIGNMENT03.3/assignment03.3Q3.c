5d95e40ff4b760088b14aa1f15bf7d1e31bbefdb
wasim180 <wa4641319@gmail.com>
2026-01-18 16:19:31 +0530

    Update assignment03.3Q3.c

--- WASIM/ASSIGNMENT03.3/assignment03.3Q3.c
+++ WASIM/ASSIGNMENT03.3/assignment03.3Q3.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-    int cp,sp,profit;
+    int cp,sp,profit,loss;
 
     printf(" Enter your cp:");
     scanf("%d",&cp);
@@ -9,12 +9,13 @@ int main(){
     scanf("%d",&sp);
 
     profit= (sp-cp);
+    loss=(cp-sp);
 
     if(sp>cp){
         printf(" profit= %d",profit);
     }
     else if(sp<cp){
-        printf(" loss=%d",profit);
+        printf(" loss=%d",loss);
     }
     else if(sp==cp){
         printf("no proft, no loss");
