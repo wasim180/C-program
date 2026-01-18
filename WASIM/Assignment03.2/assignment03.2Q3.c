da19c0297b5a2791a3cd290fcff234aa7f2b988e
wasim180 <wa4641319@gmail.com>
2026-01-18 14:45:22 +0530

    Update assignment03.2Q3.c

--- WASIM/Assignment03.2/assignment03.2Q3.c
+++ WASIM/Assignment03.2/assignment03.2Q3.c
@@ -8,16 +8,16 @@ int main(){
     if(temp<0){
         printf("Freezing weather ");
     }
-    else if(temp<10){
+    else if(temp>=0 && temp<10){
         printf("very cold weather ");
     }
-    else if(temp<20){
+    else if(temp>=10 && temp<20){
         printf("cold weather");
     }
-    else if(temp<30){
+    else if(temp>=20 && temp<30){
         printf("normal in temp");
     }
-    else if(temp<40){
+    else if( temp>=30 && temp<40){
         printf("it's hot");
     }
     else{
